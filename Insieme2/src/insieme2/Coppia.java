1d20e1a951aaba0e119bcd6e3a2b5dd3d621250f
profeSiragusano <prof.siragusano@gmail.com>
2016-06-27 21:31:19 +0200

    Aggiunti i tre file

---
+++ Insieme2/src/insieme2/Coppia.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insieme2;
+
+/**
+ *
+ * @author Carmelo
+ */
+public class Coppia {
+    private int ele1;
+    private int ele2;
+
+    public Coppia(int ele1, int ele2) {
+        this.ele1 = ele1;
+        this.ele2 = ele2;
+    }
+
+    public int getEle1() {
+        return ele1;
+    }
+
+    public void setEle1(int ele1) {
+        this.ele1 = ele1;
+    }
+
+    public int getEle2() {
+        return ele2;
+    }
+
+    public void setEle2(int ele2) {
+        this.ele2 = ele2;
+    }
+    
+    
+}
